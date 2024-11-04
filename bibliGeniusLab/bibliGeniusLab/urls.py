8004a25171f08e2e6035795b47262f50123cc9a5
Matheus Ferreira <matheusfer0902@gmail.com>
2024-11-04 20:23:06 -0300

    Criação de uma pagina home

--- bibliGeniusLab/bibliGeniusLab/urls.py
+++ bibliGeniusLab/bibliGeniusLab/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from bibliGeniusLab.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
     path('usuarios/', include('usuarios.urls')),
     path('livros/', include('livros.urls'))
 ]
--- bibliGeniusLab/usuarios/urls.py
+++ bibliGeniusLab/usuarios/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastro', views.create_usuarios, name='cadastro'),  # Rota para cadastro
+    path('cadastro', views.create_usuarios, name='cadastro_user'),  # Rota para cadastro
     path('login', views.login_view, name='login'),      # Rota para login
     path('', views.login_view, name='login_redirect'),   # Rota principal para /usuarios 
 ]
