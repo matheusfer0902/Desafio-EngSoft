8493b9219eb064ef9d9301061df91645c5583395
Matheus Ferreira <matheusfer0902@gmail.com>
2024-11-05 22:10:28 -0300

    Implementação do botão meu perfil, para funcionar como um botão de relatorio.

--- bibliGeniusLab/usuarios/urls.py
+++ bibliGeniusLab/usuarios/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('cadastro', views.create_usuarios, name='cadastro_user'),  # Rota para cadastro
     path('login', views.login_view, name='login'),      # Rota para login
-    path('', views.login_view, name='login_redirect'),   # Rota principal para /usuarios 
+    path('', views.login_view, name='login_redirect'),  # Rota principal para /usuarios
+    path('perfil', views.perfil_usuario, name='perfil_usuario'),  # Rota para a página de perfil do usuário
 ]
